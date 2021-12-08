06a6b3544783aa16843127d6cf4b467074fbbe9f
andhikaerka <andhika.ragilkesuma@gmail.com>
2021-12-08 10:17:48 +0700

    Install fortify and sanctum

--- config/app.php
+++ config/app.php
@@ -175,6 +175,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        // custom add
+        App\Providers\FortifyServiceProvider::class,
+
     ],
 
     /*
