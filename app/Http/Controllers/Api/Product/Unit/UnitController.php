1d18a53adf0de1867eeeb9b176a73f49b8886e6b
sizuwanoadi <46958807+sizuwanoadi@users.noreply.github.com>
2021-12-10 14:28:45 +0700

    feat : units (#10)
    * feat : suppliers
    
    * feat : harga jual, harga beli
    
    * feat : suppler
    
    * feat : units

---
+++ app/Http/Controllers/Api/Product/Unit/UnitController.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Http\Controllers\Api\Product\Unit;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\UnitRequest;
+use App\Models\Unit;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class UnitController extends Controller
+{
+    protected $units;
+
+    public function __construct(Unit $units)
+    {
+        $this->units = $units;
+    }
+    
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        if ($request->has('search')) {
+            $this->units = $this->units->where('name', 'LIKE', '%' . $request->search . '%');
+        }
+
+        return response()->json([
+            'success' => true,
+            'message' => 'unit list',
+            'data' => $this->units->paginate($request->length ?? 5)->appends(['search' => $request->search])
+        ], Response::HTTP_OK);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(UnitRequest $request)
+    {
+        try {
+            $this->units->create($request->validated());
+        } catch (\Exception $e) {
+            return response()->json(['message' => $e->getMessage()], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+
+        return response()->json([
+            'success' => true,
+            'message' => 'units created'
+        ], Response::HTTP_OK);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        try {
+            $unit = $this->units->findOrFail($id);
+        } catch (\Exception $e) {
+            return response()->json(['message' => $e->getMessage()], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+
+        return $unit;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UnitRequest $request, $id)
+    {
+        try {
+            $this->units->findOrFail($id)->update($request->validated());
+        } catch (\Exception $e) {
+            return response()->json(['message' => $e->getMessage()], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+
+        return response()->json([
+            'success' => true,
+            'message' => 'units updated',
+        ], Response::HTTP_OK);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        try {
+            $this->units->find($id)->delete();
+        } catch (\Exception $e) {
+            return response()->json(['message' => $e->getMessage()], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+
+        return response()->json([
+            'success' => true,
+            'message' => 'unit deleted'
+        ], Response::HTTP_OK);
+    }
+}
